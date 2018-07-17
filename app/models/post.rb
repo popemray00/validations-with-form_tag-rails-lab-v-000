36422e006b1937315be77a77692363d183e86c16
Michael Pope <popemray1986@gmail.com>
2018-07-17 15:16:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates :content, length: {minimum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
