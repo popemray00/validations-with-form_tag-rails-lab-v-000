81dd037998102b1e5235fcb75589af3cc0d950cc
Michael Pope <popemray1986@gmail.com>
2018-07-17 15:10:04 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :phone_number, length: {in: 10}
 end
